7c5b2ce4ab65552549caf0c2ffec6548f262cda3
RD <rajudhumne@gmail.com>
2018-01-11 12:30:52 +0530

    generating api key from flickr

---
+++ pixel-city/Utilities/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  pixel-city
+//
+//  Created by Raju Dhumne on 11/01/18.
+//  Copyright © 2018 Raju Dhumne. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "a12dc5dce18dfb491c1fd9d1ae69feea"
+
+func flickrUrl(forApiKey: String,withAnnotation annotation:DroppablePin,addNumberOfPhotos number:Int) -> String {
+ return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+
+}
+
